6f094826eff9d13277fdc2ba0f1d1566aac1b628
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-17 00:28:07 -0300

    Limpeza do Buffer do teclado

--- AULA 13 - Ler 2 caracteres e limpar o buffer do teclado com um espaço/main.c
+++ AULA 13 - Ler 2 caracteres e limpar o buffer do teclado com um espaço/main.c
@@ -3,6 +3,15 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char a, b;
+
+    printf("Digite dua letras: ");
+    scanf("%c %c", &a, &b);
+
+    // espaço entre o %c => é o comando para o computador desconsiderar o enter o tab e espaço.
+    // fflush(); => não usar, pois só funciona no windows. Também nçao funciona em todos os casos.
+
+    printf("Primeira letra: %c\nSegunda letra: %c\n", a, b);
+
     return 0;
 }
