5ae7c48a1be583e94b620040207c6358844f907f
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-17 22:06:54 -0300

    Unidades de medidas na computação bit byte kB MB GB TB PB EB ZB YB

---
+++ AULA 15 - Unidades de medidas na computação bit byte kB MB GB TB PB EB ZB YB/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+8 bits = 1 byte
+1024 bytes = 1 Kilobyte
+1024 Kilobytes = 1 Megabyte
+1024 Megabytes = 1 Gigabyte
+1024 Gigabytes = 1 Terabyte
+1024 Terabytes = 1 Petabyte
+1024 Petabytes = 1 Exabyte
+1024 Exabytes = 1 Zettabyte
+1024 Zettabytes =  1 Yottabyte
+
+
+*/
+
+
+int main()
+{
+
+
+    return 0;
+}
