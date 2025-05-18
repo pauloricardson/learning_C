5abcd029012b0207748dd57a6a13773f713a76f7
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-17 23:28:19 -0300

    Curso de Programação C | O que são caracteres de escape \n, \t, \", \' e \\? | aula 23

---
+++ AULA 23 - O que são caracteres de escape/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+        CARACTERES DE ESCAPE
+        Quebra de linha -> \n
+        Tabulação       -> \t
+        Aspa dupla      -> \"
+        Aspa simple     -> \'
+        Contrabarra     -> \\
+
+*/
+
+int main(){
+
+    printf("\n\n\tIsto e uma \"mensagem\" a ser impressa \\ / na tela.\n\n");
+
+    return 0;
+}
