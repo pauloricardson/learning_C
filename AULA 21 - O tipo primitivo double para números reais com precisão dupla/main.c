9b23b25d2da0abfa2ba0011df1aef7b478b2ce0c
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-17 23:08:59 -0300

    Curso de Programação C | O tipo primitivo double para números reais com precisão dupla | aula 21

---
+++ AULA 21 - O tipo primitivo double para números reais com precisão dupla/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// tipo double
+
+int main()
+{
+    float x = 3.1415; // precisão simples
+
+    long long double y = 3.14156377383928227383746882727730912; // precisão dupla
+
+    printf("Um double precisa de %d bytes de memoria\n", sizeof y);
+
+    return 0;
+}
