5f9beff9690e2f2a9ea493552e6807158d8fdc47
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-18 11:12:23 -0300

    Curso de Programação C | Como usar as operações básicas da matemática na linguagem C? | aula 24

---
+++ AULA 24 - Como usar as operações básicas da matemática na linguagem/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+
+    printf("\nSoma: %d\n", 10 + 20);
+    printf("\nSubtração: %d\n", 10 - 20);
+    printf("\nMultiplicação: %d\n", 10 * 20);
+    printf("\nDivisoo: %f\n", 10.0 / 20);
+
+    return 0;
+}
