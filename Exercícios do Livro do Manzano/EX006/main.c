3d6849bf3ef476d0ff3ab6d2d58c0ff05cdf7ec3
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-19 20:16:40 -0300

    EX007

--- Exercícios do Livro do Manzano/EX006/main.c
+++ Exercícios do Livro do Manzano/EX006/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Elabore um algoritmo que receba, por meio do teclado, dois valores, um para a variável “a” e um
+para a variável “b”. Em seguida, faça os passos que julgar necessário para que ao final, a variável
+“a” possua o valor que inicialmente estava em “b” e a variável “b” possua o valor que inicialmente
+estava em “a”. Traduza seu algoritmo para a linguagem C e exiba os valores na tela.
+*/
+
 int main(){
     int a, b, aux;
 
@@ -10,13 +17,13 @@ int main(){
     printf("Valor de B: ");
     scanf("%d", &b);
 
+    printf("\n\nValores lidos A: %d, B: %d\n\n", a, b);
+
     aux = a;
     a = b;
     b = aux;
 
     printf("\n\nValor de A: %d, Valor de B: %d\n\n", a, b);
 
-
-    printf("Hello world!\n");
     return 0;
 }
--- Exercícios do Livro do Manzano/EX007/main.c
+++ Exercícios do Livro do Manzano/EX007/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+    int a, b;
+
+    printf("Valor de A: ");
+    scanf("%d", &a);
+
+    b = a;
+
+    printf("Valor de B: ");
+    scanf("%d", &b);
+
+
+
+    printf("\n\nValor de A: %d, Valor de B: %d\n\n", a, b);
+
     return 0;
 }
