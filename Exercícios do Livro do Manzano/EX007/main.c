0c2f77ffac577b9ffb4f1c3de05933128224b642
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-19 20:38:43 -0300

    EX007
    Faça um programa em C para trocar o valor de duas variáveis inteiras sem utilizar nenhuma
    variável auxiliar.

--- Exercícios do Livro do Manzano/EX007/main.c
+++ Exercícios do Livro do Manzano/EX007/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Faça um programa em C para trocar o valor de duas variáveis inteiras sem utilizar nenhuma
+variável auxiliar.
+*/
+
 int main(){
     int a, b;
 
-    printf("Valor de A: ");
-    scanf("%d", &a);
+    printf("Valor de A e B: ");
+    scanf("%d%d", &a, &b);
 
-    b = a;
+    printf("\n\nValores lidos [%d] e [%d]\n\n", a, b);
 
-    printf("Valor de B: ");
-    scanf("%d", &b);
+    printf("Invertendo o valor fica -> A: %d e B: %d\n\n", (a + b) - a, (a + b) - b);
 
-
-
-    printf("\n\nValor de A: %d, Valor de B: %d\n\n", a, b);
 
     return 0;
 }
