025dac5eaf554aef83794282dcd8e309ba944675
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-23 21:32:07 -0300

    .

--- AULA 38 - Operadores relacionais.c
+++ AULA 38 - Operadores relacionais.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h> // PARA COMPATIBILIDADE COM PORTUGUES
 
 int main(){
+	
+	setlocale(LC_ALL, "portuguese"); // SEMPRE QUE PUDER É BOM COLOCAR
+	
 	int a = 10, b = 20;
 	
 	printf("\n\n== Valor fazer a comparação com operadores relacionais ===\nA = 10 e B = 20\nObs.: 1 - SIM, 2 - NAO\n\n");
