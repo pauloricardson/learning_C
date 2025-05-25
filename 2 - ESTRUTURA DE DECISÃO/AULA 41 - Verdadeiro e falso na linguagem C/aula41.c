78acc9374d885445dfe6af68332cda501c247c23
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2025-05-24 21:56:10 -0300

    reorganização das pastas

---
+++ 2 - ESTRUTURA DE DECISÃO/AULA 41 - Verdadeiro e falso na linguagem C/aula41.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int a;
+	
+	printf("Digite um valor qualquer: ");
+	scanf("%d", &a);
+	
+	printf("\nResultado logico: %d\n", a < 0);
+	
+	if (a < 0 == 1){ // if = se
+		printf("\n\tValor negativo!\n");
+	}else{
+		printf("\n\tO valor e positivo ou igual a 0!\n");
+	}
+	
+	printf("\nContinuacao do programa...\n");
+	
+	return 0;
+}
